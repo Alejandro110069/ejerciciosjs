aa4c1b19e9b8bcc4cb2b47b3e3c8cf6488c65943
Alejandro110069 <misael.2023110077@metropoli.edu.mx>
2026-02-15 21:16:20 -0600

    Create ej_13.js

---
+++ Tarea3/javascript/ej_13.js
@@ -0,0 +1,15 @@
+console.log(" Ejercicio 13 ");
+function removerCeros(arreglo) {
+  let nuevo = [];
+  for (let i = 0; i < arreglo.length; i++) {
+    if (arreglo[i] !== 0) {
+      nuevo.push(arreglo[i]);
+    }
+  }
+  return nuevo;
+}
+
+//Pruebas
+console.log(removerCeros([0, 1, 0, 2, 0, 3]));
+console.log(removerCeros([9, 3, 6, 4]));
+console.log(removerCeros([0, 0, 0]));
