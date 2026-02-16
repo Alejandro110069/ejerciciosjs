d7631beb2c6ead263e0f4f8d9116cf5b36cbe520
Alejandro110069 <misael.2023110077@metropoli.edu.mx>
2026-02-15 21:06:40 -0600

    Update ej_1.js

--- Tarea3/javascript/ej_1.js
+++ Tarea3/javascript/ej_1.js
@@ -1,9 +1,11 @@
 console.log(" Ejercicio 1 ");
 
 function contrasenaValida(password) {
-  return password === "sd54s1d1s2d" || password === "4sd4s6d5sd";
+  return password === "2Fj(jjbFsuj"|| password === "eoZiugBf&g9";
 }
-console.log(contrasenaValida("4sd4s6d5sd")); //true
-console.log(contrasenaValida("sd54s1d1s2d")); //true
 
-console.log(contrasenaValida("12345")); //false
+console.log(contrasenaValida("2Fj(jjbFsuj")) // true
+console.log(contrasenaValida("eoZiugBf&g9")) // true
+console.log(contrasenaValida("hola")) // false
+console.log(contrasenaValuda("")) // false
+
