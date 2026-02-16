4a43e9ee404dab0bed45a93afb98fdf07d4dfcf3
Alejandro110069 <misael.2023110077@metropoli.edu.mx>
2026-02-15 21:01:27 -0600

    Create ej_5.js

---
+++ Tarea3/javascript/ej_5.js
@@ -0,0 +1,22 @@
+console.log(" Ejercicio 5 ");
+function likes(numero) {
+
+  if (numero < 1000) {
+    return numero.toString();
+  } 
+  
+  if (numero >= 1000 && numero < 1000000) {
+    return Math.floor(numero / 1000) + "K";
+  } 
+  
+  if (numero >= 1000000) {
+    return Math.floor(numero / 1000000) + "M";
+  }
+}
+
+// Pruebas
+console.log(likes(983));      // "983"
+console.log(likes(1900));     // "1K"
+console.log(likes(54000));    // "54K"
+console.log(likes(120800));   // "120K"
+console.log(likes(25222444)); // "25M"
