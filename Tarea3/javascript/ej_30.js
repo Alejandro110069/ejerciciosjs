47d6d1cb96087534d6d3be82f7c1394bfc6c3b91
Alejandro110069 <misael.2023110077@metropoli.edu.mx>
2026-02-15 21:31:41 -0600

    Create ej_30.js

---
+++ Tarea3/javascript/ej_30.js
@@ -0,0 +1,21 @@
+console.log("===== Ejercicio 30 =====");
+
+function numAsteriscos(matriz) {
+  let contador = 0;
+  for (let i = 0; i < matriz.length; i++) {
+    for (let j = 0; j < matriz[i].length; j++) {
+      if (matriz[i][j] === "*") {
+        contador++;
+      }
+    }
+  }
+  return contador;
+}
+
+// código de prueba
+console.log(numAsteriscos([
+['*', '', '*'],
+['', '*', ''],
+['*', '', '*']
+]))
+// 5
