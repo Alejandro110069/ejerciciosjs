a75604486c1357bc858b404ef86ca5ee0a0abdb9
Alejandro110069 <misael.2023110077@metropoli.edu.mx>
2026-02-15 21:23:30 -0600

    Create ej_21.js

---
+++ Tarea3/javascript/ej_21.js
@@ -0,0 +1,14 @@
+console.log(" Ejercicio 21 ");
+
+function pares(arreglo) {
+  let resultado = [];
+  for (let i = 0; i < arreglo.length; i++) {
+    if (arreglo[i] % 2 === 0) {
+      resultado.push(arreglo[i]);
+    }
+  }
+  return resultado;
+}
+// código de prueba
+console.log(pares([1, 2, 3, 4, 5, 6])) // [2, 4, 6]
+console.log(pares([])) // []
