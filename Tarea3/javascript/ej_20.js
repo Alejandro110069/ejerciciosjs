69c15f84164fdb0b0beb9cba61365058539deec3
Alejandro110069 <misael.2023110077@metropoli.edu.mx>
2026-02-15 21:21:50 -0600

    Create ej_20.js

---
+++ Tarea3/javascript/ej_20.js
@@ -0,0 +1,16 @@
+console.log(" Ejercicio 20 ");
+
+function password(texto) {
+  return texto
+    .toLowerCase()
+    .replace(/ /g, "")
+    .replace(/a/g, "4")
+    .replace(/e/g, "3")
+    .replace(/i/g, "1")
+    .replace(/o/g, "0");
+}
+
+// código de prueba
+console.log(password("hola")) // "h0l4"
+console.log(password("esta es una prueba")) // "3st43sun4pru3b4"
+console.log(password("")) // ""
