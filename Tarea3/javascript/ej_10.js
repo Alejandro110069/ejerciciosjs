4da7bd5fd5129fb06899aa558e272e83f3670989
Alejandro110069 <misael.2023110077@metropoli.edu.mx>
2026-02-15 21:14:19 -0600

    Create ej_10.js

---
+++ Tarea3/javascript/ej_10.js
@@ -0,0 +1,18 @@
+console.log(" Ejercicio 10 ");
+function numeroDeCaracteres(texto, caracter) {
+
+  let contador = 0;
+
+  for (let i = 0; i < texto.length; i++) {
+    if (texto[i] === caracter) {
+      contador++;
+    }
+  }
+
+  return contador;
+}
+
+// Pruebas
+console.log(numeroDeCaracteres("Hola Mundo", "o")); // 2
+console.log(numeroDeCaracteres("MMMMM", "m"));      // 0
+console.log(numeroDeCaracteres("eeee", "e"));       // 4
